4359385ffae529faa6efdec731e81f70402e70ff
Tarun Jeevan <tjeevan200@gmail.com>
2023-11-19 19:21:43 -0500

    Switched to JS project. Added hero section for the main page.

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import HeroSection from './components/HeroSection'
+
+export default function Home() {
+
+    return (
+        <main className="flex min-h-screen flex-col container mx-auto px-12 py-4">
+            <HeroSection />
+        </main>
+    )
+}
