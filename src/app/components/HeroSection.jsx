475c17ea671ce6dcb46baffb173364bbd1c092c1
Tarun Jeevan <tjeevan200@gmail.com>
2023-12-08 19:24:28 -0500

    Hero section done

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,17 +1,42 @@
-import Image from "next/image";
-
+'use client'
+import Image from "next/image"
+import { TypeAnimation } from 'react-type-animation'
 
 export default function HeroSection() {
     return (
         <section>
-            <div className="grid grid-cols-1 lg:grid-cols-12">
-                <div className="col-span-7 place-self-center">
-                    <h1 className="mb-4 text-4xl lg:text-6xl font-extrabold">
-                        Hello, I&apos;m Tarun
+            <div className="grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-7 place-self-center text-center sm:text-left">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-700">Hello, I&apos;m{' '}</span>
+                        <br />
+                        <TypeAnimation
+                            sequence={[
+                                'Tarun',
+                                1000,
+                                'Web Developer',
+                                1000,
+                                'Game Designer',
+                                1000,
+                                'Game Developer',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
                     </h1>
-                    <p className="text-[#ADB7BE] text-lg lg:text-xl mb-6">
+                    <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
                         Lorem ipsum dolor...
                     </p>
+                    <div>
+                        <button className="w-full sm:w-fit font-bold bg-gradient-to-br from-blue-500 via-purple-500 to-red-500 hover:bg-slate-200 text-black px-6 py-3 rounded-full mr-4">
+                            Hire Me
+                        </button>
+                        <button className="w-full sm:w-fit font-bold bg-gradient-to-br from-blue-500 via-purple-500 to-red-500 hover:bg-slate-800 text-white mt-3 px-1 py-1 rounded-full">
+                            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
+                        </button>
+                    </div>
                 </div>
                 {/* Image */}
                 <div className="col-span-5 place-self-center mt-4 lg:mt-0">
--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,10 @@ import HeroSection from './components/HeroSection'
 export default function Home() {
 
     return (
-        <main className="flex min-h-screen flex-col container mx-auto px-12 py-4">
-            <HeroSection />
+        <main className="bg-[#121212] flex min-h-screen flex-col">
+            <div class="container mx-auto px-12 py-4">
+                <HeroSection />
+            </div>
         </main>
     )
 }
